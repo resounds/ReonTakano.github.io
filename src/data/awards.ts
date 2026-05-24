474c9a761e1db9a1eb405a8ddb3bb510a12b8a93
resound <23amj15@ms.dendai.ac.jp>
2026-05-24 15:48:20 +0900

    data: update awards with latest records

--- src/data/awards.ts
+++ src/data/awards.ts
@@ -1,7 +1,15 @@
 // src/data/awards.ts
 export const awards = [
   {
-    year: "202x",
-    title: "Innovative Research Award"
+    year: "2024",
+    title: "優秀賞, 札幌商工会議所主催ビジネスコンテスト"
+  },
+  {
+    year: "2023",
+    title: "ISS特別賞（協創）, 電子情報通信学会 総合大会"
+  },
+  {
+    year: "2023",
+    title: "ISS優秀ポスター賞, 電子情報通信学会 総合大会 ジュニア＆学生ポスターセッション"
   }
 ];
